2498e37481758099b5574e5527cee32c16432a67
akhileshsamal79 <akhilsamal9400@gmail.com>
2025-07-18 14:50:23 +0530

    Update T-1.py

--- assign-3/T-1.py
+++ assign-3/T-1.py
@@ -3,7 +3,10 @@ def factorial(n):
     for i in range(n,0,-1):
         c=x*i
         x=c
-    return c
+    if n>=2:
+        return c
+    else:
+        return 1
 n=int(input("Enter a number to calculate factorial: "))
 c=factorial(n)
 print('Factorial of ',n,'is',c)
